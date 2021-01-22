d65d25527acbd09e9e94ae65186ba27ace4d4cbb
JaiRaga <raga.j.santhosh@gmail.com>
2021-01-22 09:49:23 +0530

    displays login page

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react'
+import {makeStyles, TextField, Grid, Button} from "@material-ui/core"
+import {useDispatch, useSelector} from "react-redux"
+import {Redirect} from "react-router-dom"
+import { login } from "../../redux/actions/auth"
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        width: '25ch',
+      },
+    },
+    body :{
+        marginTop: 10
+    },
+    btnBody: {
+        marginTop: 10
+    },
+    btn: {
+        backgroundColor: "#11698e",
+        color: "#fff"
+    }
+  }));
+
+const Login = () => {
+    const classes = useStyles()
+    const dispatch = useDispatch()
+    const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+    const [login, setLogin] = useState({
+        email: '',
+        password: ''
+    })
+
+
+    const onChange = (e) => {
+        setLogin({ ...login, [e.target.name]: e.target.value })
+    }
+
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        dispatch(login(login))
+        console.log(login)
+    }
+
+    if (isAuthenticated) {
+        return <Redirect to="/home" />
+    }
+
+    return (
+        <Grid container direction="column" justify="center" alignItems="center">
+            <Grid item>
+                <Grid container item className={classes.body}>
+                <form className={classes.root} noValidate autoComplete="off">
+                    <Grid item>
+                        <TextField id="email" name="email" label="Email" variant="filled" onChange={onChange} />
+                    </Grid>
+                    <Grid item>
+                        <TextField id="password" name="password" label="Password" variant="filled" type="password" onChange={onChange} />
+                    </Grid>
+                </form>
+                </Grid>
+            </Grid>
+            <Grid item className={classes.btnBody}>
+                <Button className={classes.btn} onClick={onSubmit}>Submit</Button>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Login
