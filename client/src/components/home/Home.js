8c0ade9880e150c6a73f866ba32d099d792c692b
JaiRaga <raga.j.santhosh@gmail.com>
2021-01-31 05:43:02 +0530

    removed bgcolor

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,41 +1,46 @@
 import React, { useEffect } from 'react'
-import {Grid, makeStyles, Hidden} from "@material-ui/core"
+import { Grid, makeStyles, Hidden } from '@material-ui/core'
 import { useSelector, useDispatch } from 'react-redux'
-import Tasks from "../tasks/Tasks"
+import Tasks from '../tasks/Tasks'
 import { loadTasks } from '../../redux/actions/task'
 
-const useStyles = makeStyles(theme => ({
-    body: {
-        // backgroundColor: "#444"
-    },
-    sides: {
-        backgroundColor: "#eee"
-    }
+const useStyles = makeStyles((theme) => ({
+	root: {},
+	body: {
+		// backgroundColor: "#444"
+	},
+	sides: {
+		// backgroundColor: "#666"
+	},
 }))
 
 const Home = () => {
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(loadTasks())
-    }, [])
-    const classes = useStyles()
-    const tasks = useSelector(state => state.task.tasks)
-    
-    return (
-        <Grid container justify="center">
-            <Hidden only={['xs']}>
-            <Grid item xs={0} sm={2} md={3} className={classes.sides}>Sides</Grid>
-            </Hidden>
-            
-            <Grid item xs={12} sm={8} md={6} className={classes.body}>
-                <Tasks tasks={tasks} />
-            </Grid>
-            
-            <Hidden only={['xs']}>
-            <Grid item xs={0} sm={2} md={3} className={classes.sides}>Sides</Grid>
-            </Hidden>
-        </Grid>
-    )
+	const dispatch = useDispatch()
+	useEffect(() => {
+		dispatch(loadTasks())
+	}, [])
+	const classes = useStyles()
+	const tasks = useSelector((state) => state.task.tasks)
+
+	return (
+		<Grid container justify='center' className={classes.root}>
+			<Hidden only={['xs']}>
+				<Grid item xs={0} sm={2} md={3} className={classes.sides}>
+					Sides
+				</Grid>
+			</Hidden>
+
+			<Grid item xs={12} sm={8} md={6} className={classes.body}>
+				<Tasks tasks={tasks} />
+			</Grid>
+
+			<Hidden only={['xs']}>
+				<Grid item xs={0} sm={2} md={3} className={classes.sides}>
+					Sides
+				</Grid>
+			</Hidden>
+		</Grid>
+	)
 }
 
 export default Home
