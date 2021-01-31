750a161848ce452d6917f7b5422b5945610e6c64
JaiRaga <raga.j.santhosh@gmail.com>
2021-01-31 05:44:47 +0530

    added btns for editing and deleting tasks

--- client/src/components/tasks/TaskItem.js
+++ client/src/components/tasks/TaskItem.js
@@ -1,21 +1,61 @@
 import React from 'react'
-import { Grid, Paper, makeStyles } from "@material-ui/core"
+import { Grid, Paper, makeStyles, ButtonBase, IconButton, Typography } from "@material-ui/core"
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import EventBusyIcon from '@material-ui/icons/EventBusy';
+
 
 const useStyles = makeStyles(theme => ({
     margin: {
         margin: 10,
-        padding: 40
+        padding: '40px 10px 40px 40px',
+        marginBottom: 30,
+        backgroundColor: props => props.completed ? '#99ff99' : '#e0e0d1'
+    },
+    description: {
+        paddingTop: 15
+    },
+    editIcon: {
+        backgroundColor: theme.palette.editIcon
+    },
+    deleteIcon: {
+        backgroundColor: theme.palette.deleteIcon
     }
+    // icons: {
+    //     display: 'flex'
+    // },
+    // icon: {
+    //     marginLeft: 'auto'
+    // }
 }))
 
 const TaskItem = ({task}) => {
-    const classes = useStyles()
+    const completed = task.completed
+    const classes = useStyles({completed})
+    console.log(task.description.length, task.description)
     return (
         // <Grid container item justify="center">
         //     <Grid item>
                 <Paper elevation={11} className={classes.margin}>
-                    {task.description}
-                </Paper>
+                    <Grid container item>
+                        <Grid item xs={8}>
+                            <Typography align="center" className={classes.description}>{task.description}</Typography>
+                            </Grid>
+                        <Grid item xs={4} className={classes.icons}>
+                            <Grid container item className={classes.icon} justify="center">
+                                <Grid item>
+                                    <IconButton color='primary'>
+                                        <EventBusyIcon />
+                                    </IconButton>
+                                </Grid>
+                                <Grid item>
+                                    <IconButton color='secondary'>
+                                        <DeleteOutlineIcon />
+                                    </IconButton>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>                    
+                </Paper>            
         //     </Grid>
         // </Grid>
     )
