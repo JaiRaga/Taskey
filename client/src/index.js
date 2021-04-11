aa44ba2fb075a8b6009169d0c4f7d148d47c5630
JaiRaga <raga.j.santhosh@gmail.com>
2021-04-11 12:11:41 +0530

    passing theme to App cmp and its children

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from "@material-ui/core"
 import './index.css';
 import App from './App';
+import theme from "./theme"
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
