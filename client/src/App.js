ebd87935f72fe9b8fd8995d2956c73ad67f270a2
JaiRaga <raga.j.santhosh@gmail.com>
2021-04-11 12:12:29 +0530

    modified routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,57 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Landing from "./components/home/Landing";
-import Signup from "./components/auth/Signup";
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import PrivateRoute from './components/routing/PrivateRoute'
+import Navbar from './components/layout/Navbar'
+import Landing from './components/home/Landing'
+import Signup from './components/auth/Signup'
+import Login from './components/auth/Login'
+import Home from './components/home/Home'
+import Setting from './components/setting/Setting'
 // import Register from "./component/auth/Register";
 // import Dashboard from "./component/layout/Dashboard";
-import setAuthToken from "./utils/setAuthToken";
-// import { loadUser } from "./actions/auth";
+import setAuthToken from './utils/setAuthToken'
 
-import "./App.css";
+import './App.css'
 
 // Redux
-import { Provider } from "react-redux";
-import store from "./store";
-import Navbar from "./components/layout/Navbar";
+import { Provider } from 'react-redux'
+import store from './store'
+import { loadUser } from './redux/actions/auth'
 // import Tasks from "./component/Tasks/Tasks";
 // import Task from "./component/Tasks/Task";
 // import AddTask from "./component/Tasks/AddTask";
 // import UpdateTask from "./component/Tasks/UpdateTask";
-// import { loadTask } from "./actions/task";
+import { loadTasks } from './redux/actions/task'
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+	setAuthToken(localStorage.token)
 }
 
 function App() {
-  useEffect(() => {
-    // store.dispatch(loadUser());
-    // store.dispatch(loadTask());
-  }, []);
+	useEffect(() => {
+		store.dispatch(loadUser())
+		store.dispatch(loadTasks())
+	}, [])
 
-  return (
-    <Provider store={store}>
-      <Router>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <Switch>
-          <Route exact path='/signup' component={Signup} />
-            {/* <Route exact path='/login' component={Login} /> */}
-            {/* <Route exact path='/dashboard' component={Dashboard} />
+	return (
+		<Provider store={store}>
+			<Router>
+				<Navbar />
+				<Route exact path='/' component={Landing} />
+				<Switch>
+					<Route exact path='/signup' component={Signup} />
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/settings' component={Setting} />
+					<PrivateRoute exact path='/home' component={Home} />
+					{/* <Route exact path='/dashboard' component={Dashboard} />
             <Route exact path='/tasks' component={Tasks} />
             <Route exact path='/task/:id' component={Task} />
             <Route exact path='/add-task' component={AddTask} />
             <Route exact path='/edit-task' component={UpdateTask} /> */}
-        </Switch>
-      </Router>
-    </Provider>
-  );
+				</Switch>
+			</Router>
+		</Provider>
+	)
 }
 
-export default App;
+export default App
