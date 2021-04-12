9e560effbb135471b2f1f97e44956ccdab4b1d75
JaiRaga <raga.j.santhosh@gmail.com>
2021-04-12 15:03:34 +0530

    added key prop to task item

--- client/src/components/tasks/Tasks.js
+++ client/src/components/tasks/Tasks.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { Grid } from "@material-ui/core"
+import { Grid } from '@material-ui/core'
 import TaskItem from './TaskItem'
 
-const Tasks = ({tasks}) => {
-    return (
-            <Grid container item direction="column" justify="center">
-                <Grid item>
-                    {tasks.map(task => <TaskItem task={task} />)} 
-                </Grid>
-            </Grid>
-    )
+const Tasks = ({ tasks }) => {
+	return (
+		<Grid container item direction='column' justify='center'>
+			<Grid item>
+				{tasks.map((task) => (
+					<TaskItem key={task._id} task={task} />
+				))}
+			</Grid>
+		</Grid>
+	)
 }
 
 export default Tasks
