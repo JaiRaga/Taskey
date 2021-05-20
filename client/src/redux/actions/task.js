e52123ea22c04712a8f5a948c6215288889fd46c
JaiRaga <raga.j.santhosh@gmail.com>
2021-05-20 15:54:35 +0530

    removed log

--- client/src/redux/actions/task.js
+++ client/src/redux/actions/task.js
@@ -1,103 +1,106 @@
-import axios from "axios";
+import axios from 'axios'
 import {
-  ADD_TASK,
-  GET_TASK,
-  GET_TASKS,
-  UPDATE_TASK,
-  DELETE_TASK
-} from "./types";
+	ADD_TASK,
+	GET_TASK,
+	GET_TASKS,
+	UPDATE_TASK,
+	DELETE_TASK,
+} from './types'
 
 // Get all Tasks
 export const loadTasks = () => async (dispatch) => {
-  try {
-    const res = await axios.get("/api/tasks");
+	try {
+		const res = await axios.get(`/api/tasks?sortBy=desc`)
 
-    dispatch({
-      type: GET_TASKS,
-      payload: res.data
-    });
-    console.log(res.data)
-  } catch (err) {
-    console.log(err);
-  }
-};
+		dispatch({
+			type: GET_TASKS,
+			payload: res.data,
+		})
+		console.log(res.data)
+	} catch (err) {
+		console.log(err)
+	}
+}
 
 // Get individual Task
 export const getTask = (id) => async (dispatch) => {
-  try {
-    const res = await axios.get(`/api/tasks/${id}`);
+	try {
+		const res = await axios.get(`/api/tasks/${id}`)
 
-    dispatch({
-      type: GET_TASK,
-      payload: res.data
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+		dispatch({
+			type: GET_TASK,
+			payload: res.data,
+		})
+	} catch (err) {
+		console.log(err)
+	}
+}
 
 // Add Task
 export const addTask = (description) => async (dispatch) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  };
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}
 
-  const body = JSON.stringify({ description });
+	const body = JSON.stringify({ description })
 
-  try {
-    const res = await axios.post("/api/tasks", body, config);
+	try {
+		const res = await axios.post('/api/tasks', body, config)
 
-    dispatch({
-      type: ADD_TASK,
-      payload: res.data
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+		// console.log(res.data)
+
+		dispatch({
+			type: ADD_TASK,
+			payload: res.data,
+		})
+	} catch (err) {
+		console.log(err)
+	}
+}
 
 // Update Task
-export const updateTask = (description, completed = false, taskId) => async (
-  dispatch
-) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json"
-    }
-  };
+export const updateTask =
+	(description, completed = false, taskId) =>
+	async (dispatch) => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
 
-  let body = null;
+		let body = null
 
-  if (description) {
-    body = JSON.stringify({ description, completed });
-  } else {
-    body = JSON.stringify({ completed });
-  }
+		if (description) {
+			body = JSON.stringify({ description, completed })
+		} else {
+			body = JSON.stringify({ completed })
+		}
 
-  try {
-    const res = await axios.patch(`/api/tasks/${taskId}`, body, config);
+		try {
+			const res = await axios.patch(`/api/tasks/${taskId}`, body, config)
 
-    dispatch({
-      type: UPDATE_TASK,
-      payload: res.data
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+			dispatch({
+				type: UPDATE_TASK,
+				payload: res.data,
+			})
+		} catch (err) {
+			console.log(err)
+		}
+	}
 
 // Delete Task
 export const deleteTask = (taskId) => async (dispatch) => {
-  try {
-    const res = await axios.delete(`/api/tasks/${taskId}`);
+	try {
+		const res = await axios.delete(`/api/tasks/${taskId}`)
+		console.log(res.data)
 
-    dispatch({
-      type: DELETE_TASK,
-      payload: res.data
-    });
-  } catch (err) {
-    console.log(err);
-  }
-};
+		dispatch({
+			type: DELETE_TASK,
+			payload: res.data,
+		})
+	} catch (err) {
+		console.log(err)
+	}
+}
